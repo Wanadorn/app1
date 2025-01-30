786f548c24e32127f4d388dce36ea65fcb27278a
Wanadorn <152370529+Wanadorn@users.noreply.github.com>
2025-01-30 11:22:10 +0700

    test useContext
    method useContext class and Fuction

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { userContext } from "./context";
-import Content from "./context-content";
+import Header2 from "./context-header2";
+import Content2 from "./context-content2";
 
 
-function App() {  
+
+export default function App() {  
+  let [user,setUser] = React.useState('');
   return (
-    <userContext.Provider value={'Tom Jerry'}>
-      <Content></Content>
+    <userContext.Provider value={[user,setUser]}>
+      <Header2/>
+      <Content2/>
     </userContext.Provider>
   )
 }
 
 
-export default App;
