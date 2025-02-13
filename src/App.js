dd05a985e9da08e407aa5b2c7f48c43fbef5b675
Wanadorn <152370529+Wanadorn@users.noreply.github.com>
2025-02-13 11:43:20 +0700

    router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from "react";
-import { userContext } from "./context";
-import Header2 from "./context-header2";
-import Content2 from "./context-content2";
+import Router3 from "./router3";
 
 
 
-export default function App() {  
-  let [user,setUser] = React.useState('');
+export default function App() {   
   return (
-    <userContext.Provider value={[user,setUser]}>
-      <Header2/>
-      <Content2/>
-    </userContext.Provider>
+    <Router3/>   
   )
 }
 
